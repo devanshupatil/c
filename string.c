b4b087992f17933daf4723c11b1aef91105570f3
Devnashu Patil <devanshupatil692@gmail.com>
2023-07-09 11:49:22 +0530

    string

---
+++ string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stralign.h>
+
+int main()
+{
+    char firstName[10];
+    char lastName[10];
+
+    printf("Enter string 1 :");
+    scanf("%c", &firstName);
+
+    printf("Enter string 2 :");
+    scanf("%c", &lastName);
+
+    for (int i = 0; firstName[i] != '\0'; i++)
+    {
+
+        printf("fristName = %c", firstName[i]);
+    }
+
+    for (int j = 0; lastName[j] != '\0'; j++)
+    {
+
+        printf("LastNumber = %c", lastName[j]);
+    }
+    printf("\n");
+    return 0;
+}
