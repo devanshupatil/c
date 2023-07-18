c353a6981f2b9a48a9420c66e6167db9abad7ad3
Devnashu Patil <devanshupatil692@gmail.com>
2023-07-18 16:48:52 +0530

    REACH_HOME

---
+++ Code_Chef_Problems/Reach Home.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int T;
+    int X;
+    int Y;
+
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++)
+    {
+        scanf("%d %d", &X, &Y);
+
+        if (5 * X >= Y)
+        {
+            printf("YES \n");
+        }
+
+        else
+        {
+            printf("NO \n");
+        }
+    }
+
+    return 0;
+}
