bae19f2bde8ca4da65885b38f609777d2076a501
Devnashu Patil <devanshupatil692@gmail.com>
2023-07-19 21:55:20 +0530

    Square_Number.c

---
+++ Basic_Problem_Solving/Square_Number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number :");
+    scanf("%d", &n);
+
+    printf("The square of number is = %f", pow(n, 2));
+    return 0;
+}
