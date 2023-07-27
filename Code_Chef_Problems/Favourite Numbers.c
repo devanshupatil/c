17e860ae56e2c6b26e7e3c6650d0977d84c49b2a
Devnashu Patil <devanshupatil692@gmail.com>
2023-07-27 23:04:33 +0530

    Favourite Numbers

---
+++ Code_Chef_Problems/Favourite Numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int T; // Denoting the number of test cases
+    int A; // Hole number
+
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) // The run each tast Case
+    {
+        scanf("%d", &A);
+
+        if (A % 2 == 0 && A % 7 == 0) //  The numbers which are even, and are a multiple of 7
+        {
+            printf("Alice \n"); // If true , print 'Alice'
+        }
+        else if (A % 2 == 1 && A % 9 == 0) // The numbers which are odd, and are a multiple of 9
+        {
+            printf("Bob \n"); // If true , print 'Bob'
+        }
+        else
+        {
+            printf("Charile \n"); // If false , print 'Charile'
+        }
+    }
+    return 0; // Exit the program
+}
