422f2219ace25fcc31dbcd570b635504dbd4d772
Devnashu Patil <devanshupatil692@gmail.com>
2023-08-20 20:16:27 +0530

    logic change

--- Basic_Problem_Solving/calculated.c
+++ Basic_Problem_Solving/calculated.c
@@ -31,69 +31,39 @@ int main()
     printf("Enter how may day you have to add : ");
     scanf("%d", &add_day);
 
-    printf("%d-%d-%d\n", c1.day, c1.month, c1.year);
+    printf("The current date = %d-%d-%d\n", c1.day, c1.month, c1.year);
 
-    if (c1.day <= arr[c1.month - 1]) // If day is less than vaild day in month
+    if (c1.day <= arr[c1.month - 1]) // If day is less than or equal to valid day in month
     {
         while (add_day != 0) // If true , add of day is not equal to 0
         {
-            if (c1.day == arr[c1.month - 1]) // If true , day is equal to valid day in month
+            if (c1.month == 12) // If month is equal to 12
             {
-
-                if (c1.month == 12) // If true , month is equal to 12
+                if (c1.day == arr[c1.month - 1]) // If true , if day is equal to valid day in month
                 {
-                    if (c1.day == arr[c1.month - 1]) // If true , day is equal to vaild day on month
-                    {
-                        c1.year++;                        // If true , Add year by one
-                        c1.month = 1;                     // If true , assign 1
-                        c1.day = 1;                       // If true , assign 1
-                        for (int j = 1; j < add_day; j++) // If true , Run each day we have to add
-                        {
-                            c1.day++; // If true , Add day by one
-
-                            if (c1.day == arr[c1.month - 1])
-                            {
-                                c1.month++;
-                                c1.day = 1;
-                            }
-                        }
-                        printf("%d-%d-%d\n", c1.day, c1.month, c1.year); // If true , print 'day-month-year'
-                        return 0;                                        // If true , Exit the program
-                    }
-                    else
-                    {
-                        for (int j = 1; j < add_day; j++) // If false , Run each day we have to add
-                        {
-                            c1.day++; // Add day by one
-                        }
-                        printf("%d-%d-%d\n", c1.day, c1.month, c1.year); // If false , print 'day-month-year'
-                        return 0;                                        // Exit the program
-                    }
+                    c1.year++;    // If true , add by one
+                    c1.month = 1; // If true , assign 1
+                    c1.day = 0;   // If true , assign 0
                 }
-                else
-                {
-                    c1.month++;                       // If false , Add month by one
-                    c1.day = 1;                       // If false , Assign 1
-                    for (int j = 1; j < add_day; j++) // If false , Run each day we have to add
-                    {
-                        c1.day++; // If false , Add day by one
-                    }
-                    printf("%d-%d-%d\n", c1.day, c1.month, c1.year); // If false ,print 'day-month-year'
-                    return 0;                                        // If false ,Exit the program
-                }
+            }
+            else if (c1.day == arr[c1.month - 1]) // If day is equal to valid day in month
+            {
+                c1.month++; // If true , add by one
+                c1.day = 0; // If true , assign 0
             }
             else
             {
-                c1.day++;  // If false , Add day by one
-                add_day--; // If false ,Subtract add of day by one
+                c1.day++;  // If false , add by one
+                add_day--; // If false , sudtract by one
             }
         }
-        printf("%d-%d-%d\n", c1.day, c1.month, c1.year); // If false ,print 'day-month-year'
-        return 0;                                        // If false ,Exit the program
+
+        printf("The added date = %d-%d-%d\n", c1.day, c1.month, c1.year); // If true , print 'Date'
     }
     else
     {
-        printf("Not valid day in a month\n"); // If false , print 'Not valid day in a month'
+        printf("Not valid day in a month.\n"); // If false , print 'Not valid day in a month.'
     }
+
     return 0; // Exit the program
 }
