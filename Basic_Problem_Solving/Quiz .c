c0357b8c5df6b1f2e40831fa27737f77e1e099d2
Devnashu Patil <devanshupatil692@gmail.com>
2023-09-04 21:37:29 +0530

    MCQ Questions

--- Basic_Problem_Solving/Quiz .c
+++ Basic_Problem_Solving/Quiz .c
@@ -1,154 +1,93 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Quiz
+struct Question
 {
-    char answer;
+    char question[100];
+    char A[100];
+    char B[100];
+    char C[100];
+    char D[100];
+    char answer[100];
+    char ans;
+};
+
+struct Quizs
+{
+    struct Question Questions[100];
 };
 
 int main()
 {
-    struct Quiz questions;
+    struct Quizs quiz;
 
     int count = 0;
 
-    printf("Q1. Which one of the following method of revenue assessment is related to the Vijayanagara Empire?\n");
-    printf("A. Chauth\n");
-    printf("B. Ryotwari\n");
-    printf("C. Rae Rekho\n");
-    printf("D. Sardeshmukhi\n"); //
-    printf("\n");
+    strcpy(quiz.Questions[0].question, "Q1. Which one of the following method of revenue assessment is related to the Vijayanagara Empire?");
 
-    printf("Enter Answe :");
+    strcpy(quiz.Questions[0].A, "A. Chauth");
+    strcpy(quiz.Questions[0].B, "B. Ryotwari");
+    strcpy(quiz.Questions[0].C, "C. Rae Rekho");
+    strcpy(quiz.Questions[0].D, "D. Sardeshmukhi");
+    strcpy(quiz.Questions[0].answer, "D. Sardeshmukhi");
 
-    scanf("%c", &questions.answer);
+    strcpy(quiz.Questions[1].question, "Q2. Chhatrapati Shivaji Maharaj crowned himself at");
 
-    if (questions.answer == 'D')
+    strcpy(quiz.Questions[1].A, "A. Puna");
+    strcpy(quiz.Questions[1].B, "B. Konkan");
+    strcpy(quiz.Questions[1].C, "C. Bijapur");
+    strcpy(quiz.Questions[1].D, "D. Raigarh");
+    strcpy(quiz.Questions[1].answer, "D. Raigarh");
+
+    strcpy(quiz.Questions[2].question, "Q3. Chhatrapati Shivaji Maharaj maintained a fleet at");
+
+    strcpy(quiz.Questions[2].A, "A. Surat");
+    strcpy(quiz.Questions[2].B, "B. Calicut");
+    strcpy(quiz.Questions[2].C, "C. Bombay");
+    strcpy(quiz.Questions[2].D, "D. Kolaba");
+    strcpy(quiz.Questions[2].answer, "D. Kolaba");
+
+    strcpy(quiz.Questions[3].question, "Q4. Chhatrapati Shivaji Maharaj submitted to Jai Singh and signed the treaty of\n");
+
+    strcpy(quiz.Questions[3].A, "A. Puna");
+    strcpy(quiz.Questions[3].B, "B. Purandhar");
+    strcpy(quiz.Questions[3].C, "C. Konkan");
+    strcpy(quiz.Questions[3].D, "D. Surat");
+    strcpy(quiz.Questions[3].answer, "B. Purandhar");
+
+    strcpy(quiz.Questions[4].question, "Q5. Under Chhatrapati Shivaji Maharaj rule, the head of a unit of 25 in the Cavalry was known as");
+
+    strcpy(quiz.Questions[4].A, "A. Jumadar");
+    strcpy(quiz.Questions[4].B, "B. Havaldar");
+    strcpy(quiz.Questions[4].C, "C. Faujdar");
+    strcpy(quiz.Questions[4].D, "D. Hazari");
+    strcpy(quiz.Questions[4].answer, "B. Havaldar");
+
+    for (int i = 0; i < 5; i++)
     {
+        puts(quiz.Questions[i].question);
+        puts(quiz.Questions[i].A);
+        puts(quiz.Questions[i].B);
+        puts(quiz.Questions[i].C);
+        puts(quiz.Questions[i].D);
+
         printf("\n");
-        printf("Your answer is correct.\n");
+        printf("Enter Answer : ");
+        scanf("%c", &quiz.Questions[i].ans);
+        getchar();
+
+        if (quiz.Questions[i].ans == quiz.Questions[i].answer[0])
+        {
+            printf("Your answer is correct\n");
+            count++;
+        }
+        else
+        {
+            printf("Your answer is not correct. Correct answer was %s.\n", quiz.Questions[i].answer);
+        }
+
         printf("\n");
-        printf("Explanation : ‘Sardeshmukhi’ is an additional 10% tax levied upon the collected ‘Chauth’. The reasons for the additional tax was due to the King claiming hereditary rights upon the tax collection. This revenue assessment is related to the Vijayanagara Empire.\n");
-        count++;
     }
-    else
-    {
-        printf("Your answer is not correct. Correct answer was D\n");
-    }
-
-    printf("\n");
-    printf("\n");
-
-    printf("Q2. Chhatrapati Shivaji Maharaj crowned himself at\n");
-    printf("A. Poona\n");
-    printf("B. Konkan\n");
-    printf("C. Bijapur\n");
-    printf("D. Raigarh\n"); //
-    printf("\n");
-
-    printf("Enter Answe :");
-
-    getchar();
-
-    scanf("%c", &questions.answer);
-
-    if (questions.answer == 'D')
-    {
-        printf("\n");
-        printf("Your answer is correct.\n");
-        count++;
-    }
-    else
-    {
-        printf("Your answer is not correct. Correct answer was D\n");
-    }
-
-    printf("\n");
-    printf("\n");
-
-    printf("Q3. Chhatrapati Shivaji Maharaj maintained a fleet at\n");
-    printf("A. Surat\n");
-    printf("B. Calicut\n");
-    printf("C. Bombay\n");
-    printf("D. Kolaba\n"); //
-    printf("\n");
-
-    printf("Enter Answe :");
-    getchar();
-
-    scanf("%c", &questions.answer);
-
-    if (questions.answer == 'D')
-    {
-        printf("\n");
-        printf("Your answer is correct.\n");
-        printf("\n");
-        printf("Explanation : Chhatrapati Shivaji Maharaj maintained a fleet at Kolaba to plunder the Mughal trading vessels and also to check the pirates.\n");
-        count++;
-    }
-    else
-    {
-        printf("Your answer is not correct. Correct answer was D\n");
-    }
-
-    printf("\n");
-    printf("\n");
-
-    printf("Q4. Chhatrapati Shivaji Maharaj submitted to Jai Singh and signed the treaty of\n");
-    printf("A. Poona\n");
-    printf("B. Purandhar\n"); //
-    printf("C. Konkan\n");
-    printf("D. Surat\n");
-    printf("\n");
-
-    printf("Enter Answe :");
-    getchar();
-
-    scanf("%c", &questions.answer);
-
-    if (questions.answer == 'B')
-    {
-        printf("\n");
-        printf("Your answer is correct.\n");
-        printf("\n");
-        printf("Explanation : The Treaty of Purandar was signed on June 11, 1665, between the Rajput ruler Jai Singh I, who was commander of the Mughal Empire, and Maratha Chhatrapati Shivaji Maharaj. Shivaji was forced to sign the agreement after Jai Singh besieged Purandar fort.\n");
-        count++;
-    }
-    else
-    {
-        printf("Your answer is not correct. Correct answer was B\n");
-    }
-
-    printf("\n");
-    printf("\n");
-
-    printf("Q5. Under Chhatrapati Shivaji Maharaj rule, the head of a unit of 25 in the Cavalry was known as\n");
-    printf("A. Jumadar\n");
-    printf("B. Havaldar\n"); //
-    printf("C. Faujdar\n");
-    printf("D. Hazari\n");
-    printf("\n");
-
-    printf("Enter Answe :");
-
-    getchar();
-    scanf("%c", &questions.answer);
-
-    if (questions.answer == 'B')
-    {
-        printf("\n");
-        printf("Your answer is correct.\n");
-        printf("\n");
-        printf("Explanation : One unit in the cavalry was formed of 25 troopers; over them was placed a havaldar, and five havaldars formed one jumla under a jumladar. Ten jumladars made a hazari charge, and five hazaris were placed under a panjhazari, who was given a salary of 2000 huns.\n");
-        count++;
-    }
-    else
-    {
-        printf("Your answer is not correct. Correct answer was B\n");
-    }
-
-    printf("\n");
-    printf("\n");
 
     printf("Your total score is  5 / %d\n", count);
     return 0;
