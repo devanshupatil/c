98f23a06c36e356cb7979d2b99ed8acc811fc1f5
Devnashu Patil <devanshupatil692@gmail.com>
2023-09-30 03:51:12 +0530

    Quiz Game

--- File/Quiz_game.c
+++ File/Quiz_game.c
@@ -24,156 +24,175 @@ int main()
     FILE *ptr;
     ptr = fopen("C:\\Users\\Dell\\Desktop\\Question and Answer.txt", "r");
 
-    char Ans;
+    char ch;
     int breaks = 0;
+    int Question = 0;
     int count = 0;
-    int question = 0;
+    char Q = 49;
+    int i = 0;
+    int j = 0;
     struct Quizs quiz;
 
-    for (int i = 0; i < 6; i++)
+    ch = getc(ptr);
+    while (ch != EOF) // EOF
     {
         while (quiz.Questions[i].question[0] != 'Q')
         {
-            fscanf(ptr, "%c", &quiz.Questions[i].question[0]);
+            quiz.Questions[i].question[0] = ch;
+            ch = getc(ptr);
         }
 
         if (quiz.Questions[i].question[0] == 'Q')
         {
-            question++;
-            quiz.Questions[i].question[1] = '.';
+            quiz.Questions[i].question[1] = Q;
+            Q++;
+            Question++;
+            quiz.Questions[i].question[2] = '.';
+            quiz.Questions[i].question[3] = ' ';
 
-            for (int j = 2; j < 1000; j++)
+            j = 4;
+            while (j != 1000)
             {
-                fscanf(ptr, "%c", &quiz.Questions[i].question[j]);
-
-                if (quiz.Questions[i].question[j] == 123)
-                {
-                    quiz.Questions[i].question[j] = question;
-                    quiz.Questions[i].question[j + 1] = ' ';
-                    j++;
-                }
-                else if (quiz.Questions[i].question[j] == '.')
+                if (ch == 125)
                 {
                     break;
                 }
+                else if (ch != 123)
+                {
+                    quiz.Questions[i].question[j] = ch;
+                    j++;
+                }
+                ch = getc(ptr);
             }
         }
 
         while (quiz.Questions[i].Option_A[0] != 'O')
         {
-            fscanf(ptr, "%c", &quiz.Questions[i].Option_A[0]);
+            quiz.Questions[i].Option_A[0] = ch;
+            ch = getc(ptr);
         }
 
         if (quiz.Questions[i].Option_A[0] == 'O')
         {
-            for (int j = 0; j < 100; j++)
+            j = 3;
+            quiz.Questions[i].Option_A[0] = 'A';
+            quiz.Questions[i].Option_A[1] = '.';
+            quiz.Questions[i].Option_A[2] = ' ';
+
+            while (j != 100)
             {
-                fscanf(ptr, "%c", &quiz.Questions[i].Option_A[j]);
 
-                if (quiz.Questions[i].Option_A[j] == 123)
+                if (ch != 123)
                 {
-                    quiz.Questions[i].Option_A[j] = 'A';
-                    quiz.Questions[i].Option_A[j + 1] = '.';
-                    quiz.Questions[i].Option_A[j + 2] = ' ';
-                    j += 2;
-                }
-                else if (quiz.Questions[i].Option_A[j] == ',')
-                {
-                    quiz.Questions[i].Option_A[j] = '.';
-
-                    if (quiz.Questions[i].Option_A[j] == '.')
+                    if (ch != ',')
                     {
+                        quiz.Questions[i].Option_A[j] = ch;
+                        j++;
+                    }
+
+                    if (ch == ',')
+                    {
+                        j = 3;
                         quiz.Questions[i].Option_B[0] = 'B';
                         quiz.Questions[i].Option_B[1] = '.';
                         quiz.Questions[i].Option_B[2] = ' ';
-                    }
-
-                    for (int k = 3; k < 100; k++)
-                    {
-                        fscanf(ptr, "%c", &quiz.Questions[i].Option_B[k]);
-
-                        if (quiz.Questions[i].Option_B[k] == ',')
+                        ch = getc(ptr);
+                        while (j != 100)
                         {
-                            quiz.Questions[i].Option_B[k] = '.';
-
-                            if (quiz.Questions[i].Option_B[k] == '.')
+                            if (ch != ',')
                             {
+                                quiz.Questions[i].Option_B[j] = ch;
+                                ch = getc(ptr);
+                                j++;
+                            }
+                            else
+                            {
+                                j = 3;
                                 quiz.Questions[i].Option_C[0] = 'C';
                                 quiz.Questions[i].Option_C[1] = '.';
                                 quiz.Questions[i].Option_C[2] = ' ';
-                            }
+                                ch = getc(ptr);
 
-                            for (int l = 3; l < 100; l++)
-                            {
-                                fscanf(ptr, "%c", &quiz.Questions[i].Option_C[l]);
-
-                                if (quiz.Questions[i].Option_C[l] == ',')
+                                while (j != 100)
                                 {
-                                    quiz.Questions[i].Option_C[l] = '.';
-
-                                    if (quiz.Questions[i].Option_C[l] == '.')
+                                    if (ch != ',')
                                     {
+                                        quiz.Questions[i].Option_C[j] = ch;
+                                        ch = getc(ptr);
+                                        j++;
+                                    }
+                                    else
+                                    {
+                                        j = 3;
                                         quiz.Questions[i].Option_D[0] = 'D';
                                         quiz.Questions[i].Option_D[1] = '.';
                                         quiz.Questions[i].Option_D[2] = ' ';
-                                    }
-                                    for (int l = 3; l < 100; l++)
-                                    {
-                                        fscanf(ptr, "%c", &quiz.Questions[i].Option_D[l]);
+                                        ch = getc(ptr);
 
-                                        if (quiz.Questions[i].Option_D[l] == 125)
+                                        while (j != 100)
                                         {
-                                            breaks = l;
-                                            quiz.Questions[i].Option_D[l] = '.';
-                                            break;
+                                            if (ch != 125)
+                                            {
+                                                quiz.Questions[i].Option_D[j] = ch;
+                                                ch = getc(ptr);
+                                                j++;
+                                            }
+                                            else
+                                            {
+                                                break;
+                                            }
                                         }
                                     }
-                                }
-                                if (quiz.Questions[i].Option_D[breaks] == '.')
-                                {
-                                    break;
+
+                                    if (ch == 125)
+                                    {
+                                        break;
+                                    }
                                 }
                             }
-                        }
-                        if (quiz.Questions[i].Option_D[breaks] == '.')
-                        {
-                            break;
+
+                            if (ch == 125)
+                            {
+                                break;
+                            }
                         }
                     }
+                    if (ch == 125)
+                    {
+                        break;
+                    }
                 }
-                if (quiz.Questions[i].Option_D[breaks] == '.')
-                {
-                    break;
-                }
+                ch = getc(ptr);
             }
-        }
 
-        while (quiz.Questions[i].answer[0] != 'A')
-        {
-            fscanf(ptr, "%c", &quiz.Questions[i].answer[0]);
-        }
-
-        if (quiz.Questions[i].answer[0] == 'A')
-        {
-            for (int z = 0; z < 100; z++)
+            while (quiz.Questions[i].answer[0] != 'A')
             {
-                fscanf(ptr, "%c", &quiz.Questions[i].answer[z]);
 
-                if (quiz.Questions[i].answer[z] == 123)
-                {
-                    quiz.Questions[i].answer[z] = ' ';
-                }
-                else if (quiz.Questions[i].answer[z] == 125)
-                {
-                    quiz.Questions[i].answer[z] = '.';
-                    break;
-                }
+                quiz.Questions[i].answer[0] = ch;
+                ch = getc(ptr);
             }
+
+            j = 0;
+            while (ch != 125)
+            {
+                if (ch != 123)
+                {
+                    if (ch != 125)
+                    {
+                        quiz.Questions[i].answer[j] = ch;
+                        j++;
+                    }
+                }
+                ch = getc(ptr);
+            }
+            i++;
+            ch = getc(ptr);
         }
     }
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < Question; i++)
     {
+        char Ans;
         puts(quiz.Questions[i].question);
         puts(quiz.Questions[i].Option_A);
         puts(quiz.Questions[i].Option_B);
@@ -187,7 +206,7 @@ int main()
 
         Ans = toupper(Ans);
 
-        if (Ans == quiz.Questions[i].answer[1])
+        if (Ans == quiz.Questions[i].answer[0])
         {
             printf("Your answer is correct\n");
             count++;
@@ -200,8 +219,6 @@ int main()
         printf("\n");
     }
 
-    printf("Your total score is  6 / %d\n", count);
+    printf("Your total score is %d / %d\n", count, Question);
     fclose(ptr);
-
-    return 0;
 }
