df49f0de403395409943fc1eff43b67065e3dff5
Devnashu Patil <devanshupatil692@gmail.com>
2023-10-06 19:49:45 +0530

    remove

--- Basic_Problem_Solving/simple_list.c
+++ Basic_Problem_Solving/simple_list.c
@@ -1,58 +1,100 @@
 #include <stdio.h>
 
-struct List
+struct Data
 {
     int integer;
     char Character;
     float floats;
 };
 
+struct List_Item
+{
+    struct Data data;
+    int type;
+};
+
+struct List // Array
+{
+    int Capacity;
+    int index_number;
+    int *ptr;
+};
+
+void CreateList() // Create arrya requ size and assign to pointer of size
+{
+}
+
+// grow list
+
+void getInput();
+void listSize();
+
+int EnterInteger()
+{
+    struct List_Item type[100];
+    printf("Enter integer value : ");
+    scanf("%d");
+}
+
+char EnterCharacter()
+{
+    struct List_Item type[100];
+    printf("Enter character value : ");
+    scanf("%c", &type[index_number].data.Character);
+}
+
+float EnterFloat()
+{
+    struct List_Item type[100];
+    printf("Enter float value : ");
+    scanf("%f", &type[index_number].data.floats);
+}
+
 int main()
 {
-    struct List type[100];
-
-    int Number;
-    int index_number = 0;
-
+    struct List_Item type[100];
     printf("0. Exit  1. char  2. int  3. float\n");
     printf("*****************************\n");
-    printf("Enter number of data type : ");
-    scanf("%d", &Number); // Taking input
+    getInput();
 
-    while (Number != 0) // If number if not equal to 0
+    while (type[index_number].type != 0)
     {
-        if (index_number == 100) // If index number is equal to 100
+        if (index_number == Capacity)
         {
-            index_number += 100;            // If true, Add 100 into index number variable
-            struct List type[index_number]; // If true, Increasing struct size by 100
+            listSize();
         }
-        else if (Number == 1) // If the number is 1
+        else if (type[index_number].type == 1)
         {
-            printf("Enter integer value : ");
-            scanf("%d", &type[index_number].integer); // If true, Take input in struct integer
-            index_number++;                           // If true, Index number add by one
+            EnterInteger();
         }
-        else if (Number == 2) // If the number is 2
+        else if (type[index_number].type == 2)
         {
-            printf("Enter character value : ");
             getchar();
-            scanf("%c", &type[index_number].Character); // If true, Take input in struct character
-            index_number++;                             // If true, Index number add by one
+            EnterCharacter();
         }
-        else if (Number == 3) // If the number is 3
+        else if (type[index_number].type == 3)
         {
-            printf("Enter float value : ");
-            scanf("%f", &type[index_number].floats); // If true, Take input in struct float
-            index_number++;                          // If true, Index number add by one
+            EnterFloat();
         }
         else
         {
-            printf("INVALID NUMBER!\n"); // If false, print 'INVALID NUMBER!'
+            printf("INVALID NUMBER!\n");
         }
 
-        printf("Enter number of data type : ");
-        scanf("%d", &Number); // Taking input
+        index_number++;
+        getInput();
     }
 
-    return 0; // Exit the program
+    return 0;
 }
+
+void getInput()
+{
+    struct List_Item type[100];
+    printf("Enter number of data type : ");
+    scanf("%d", &type[index_number].type);
+}
+
+// void listSize()
+// {
+// }
