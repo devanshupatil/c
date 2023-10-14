4c1c5bc7f200f24ca498e1d217360e203ac0dfd4
Devnashu Patil <devanshupatil692@gmail.com>
2023-10-15 05:01:59 +0530

    Simple List

--- Basic_Problem_Solving/simple_list.c
+++ Basic_Problem_Solving/simple_list.c
@@ -8,7 +8,7 @@ struct Data
     float floats;
 };
 
-struct List_Items
+struct List_Item
 {
     struct Data data;
     int type;
@@ -18,84 +18,117 @@ struct List // Array
 {
     int Capacity; // Increasing
     int index_number;
-    int *ptr;
+    struct List_Item *ptr;
 };
 
-struct List_Items item[100];
-struct List list;
-
-void getInput()
+void add(struct List *list)
 {
-    printf("Enetr type : ");
-    scanf("%d", &item[list.index_number].type);
-}
+    struct List_Item *tem_ptr;
+    list->Capacity += 5;
+    tem_ptr = (struct List *)malloc(list->Capacity * sizeof(struct List)); // New list
 
-void add()
-{
-    list.Capacity += 5;
-    list.ptr = (struct List_Items *)malloc(list.Capacity * sizeof(struct List_Items));
-    getInput();
-}
-
-int typeInteger()
-{
-    printf("Enetr integer : ");
-    scanf("%d", &item[list.index_number].data.integer);
-}
-
-char typeCharacter()
-{
-
-    printf("Enetr Character: ");
-    scanf("%d", &item[list.index_number].data.Character);
-}
-
-float typeFloat()
-{
-
-    printf("Enetr float : ");
-    scanf("%d", &item[list.index_number].data.floats);
-}
-
-int main()
-{
-    // create list, function
-    list.index_number = 0;
-    list.Capacity = 1;
-
-    printf("0. Exit  1. Integer  2. character  3. Float\n");
-    printf("********************************************\n");
-    getInput();
-
-    while (list.index_number != list.Capacity)
+    // Assigning old list element to new list element
+    for (int i = 0; i < list[0].index_number; i++)
     {
-        if (item[list.index_number].type == 1)
-        {
-            typeInteger();
-        }
-        else if (item[list.index_number].type == 2)
-        {
-            typeCharacter();
-        }
-        else if (item[list.index_number].type == 3)
-        {
-        }
-        else if (item[list.index_number].type == 0)
-        {
-            break;
-        }
+        tem_ptr[i].type = list->ptr[i].type;
 
-        list.index_number++;
-
-        if (list.index_number == list.Capacity)
+        if (list->ptr[i].type == 1)
         {
-            add();
+            tem_ptr[i].data.integer = list->ptr[i].data.integer;
         }
-        else if (list.index_number != list.Capacity)
+        if (list->ptr[i].type == 2)
         {
-            getInput();
+            tem_ptr[i].data.Character = list->ptr[i].data.Character;
+        }
+        if (list->ptr[i].type == 3)
+        {
+            tem_ptr[i].data.floats = list->ptr[i].data.floats;
         }
     }
 
+    free(list->ptr);
+    list->ptr = tem_ptr;
+}
+void typeInteger(struct List *list)
+{
+    list->ptr->type = 1;
+    printf("Enetr integer : ");
+    scanf("%d", &list->ptr[list->index_number].data.integer);
+}
+
+void typeCharacter(struct List *list)
+{
+    list->ptr->type = 2;
+    printf("Enetr Character: ");
+    scanf("%d", &list->ptr[list->index_number].data.Character);
+}
+
+void typeFloat(struct List *list)
+{
+    list->ptr->type = 3;
+    printf("Enetr float : ");
+    scanf("%d", &list->ptr[list->index_number].data.floats);
+}
+
+void print_list(struct List *list)
+{
+    for (int i = 0; i < list->index_number + 1; i++)
+    {
+        printf("Type: %d\n", list->ptr[i].type);
+
+        if (list->ptr[i].type == 1)
+        {
+            printf("Value: %d\n", list->ptr[i].data.integer);
+        }
+        else if (list->ptr[i].type == 2)
+        {
+            printf("Character: %c\n", list->ptr[i].data.Character);
+        }
+        else if (list->ptr[i].type == 3)
+        {
+            printf("Float: %d\n", list->ptr[i].data.floats);
+        }
+    }
+}
+int main()
+{
+    struct List list;
+
+    list.index_number = 0;
+    list.Capacity = 1;
+    int data_type;
+
+    list.ptr = (struct List *)malloc(list.Capacity * sizeof(struct List));
+
+    while (1)
+    {
+        printf("0. Exit  1. Integer  2. character  3. Float\n");
+        printf("********************************************\n");
+        printf("Enetr type : ");
+        scanf("%d", &data_type);
+
+        if (data_type == 0)
+        {
+            break;
+        }
+        if (list.index_number == list.Capacity)
+        {
+            add(&list);
+        }
+        if (data_type == 1)
+        {
+            typeInteger(&list);
+        }
+        if (data_type == 2)
+        {
+            typeCharacter(&list);
+        }
+        if (data_type == 3)
+        {
+        }
+
+        print_list(&list);
+        list.index_number++;
+    }
     return 0;
 }
