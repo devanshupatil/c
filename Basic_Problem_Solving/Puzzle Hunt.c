6b5da18e0d0666a8d42a25f2a6ad94361883528f
Devnashu Patil <devanshupatil692@gmail.com>
2023-11-13 17:55:49 +0530

    Puzzle Hunt

---
+++ Basic_Problem_Solving/Puzzle Hunt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+
+    scanf("%d", &N);
+
+    if (N >= 6 && N <= 8)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
