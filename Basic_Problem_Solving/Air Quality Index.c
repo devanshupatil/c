f924283397bd1f63bf03e0585e1d8678d019b228
Devnashu Patil <devanshupatil692@gmail.com>
2023-11-13 18:22:34 +0530

    Air Quality Index

---
+++ Basic_Problem_Solving/Air Quality Index.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+
+    scanf("%d", &N);
+
+    if (N < 100)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
