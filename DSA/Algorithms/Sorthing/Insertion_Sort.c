a8d545cddea7874f6be776190d9a077209f6dc60
Devnashu Patil <devanshupatil692@gmail.com>
2024-04-29 16:03:32 +0530

    Insertion Sort

--- DSA/Algorithms/Sorthing/Insertion_Sort.c
+++ DSA/Algorithms/Sorthing/Insertion_Sort.c
@@ -4,34 +4,30 @@ int main()
 {
     int arr[] = {16, 78, 6, 8, 5, 90, 876, 67698, 9043, 10};
     int size = sizeof(arr) / sizeof(int);
-    int pivot = 16;
-    int temp;
-    int index;
 
+    printf("UnSorted Array :-\n");
     for (int i = 0; i < size; i++)
     {
-        if (pivot < arr[i])
-        {
-            for (int j = size - 1; j >= 0; j--)
-            {
-                if (i <= j)
-                {
-                    index = i;
-                }
-                else if (j <= i)
-                {
-                    index = j;
-                }
-                if (arr[j] < pivot)
-                {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+        printf("%d \t", arr[i]);
     }
 
-    printf("The element %d is sorted in index %d", pivot, index);
+    for (int i = 1; i < size; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+        while (arr[j] > current && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+
+    printf("\n Sorted Array :-\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d \t", arr[i]);
+    }
+
     return 0;
 }
